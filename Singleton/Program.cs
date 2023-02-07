88a90093c045581f6b1b0e1425ba8e8591cd3d62
Semih Özdamar <semih.oezdamar@stud.tu-darmstadt.de>
2023-02-07 21:45:29 +0100

    Add project files.

---
+++ Singleton/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var customerManager = CustomerManager.CreateSingleton();
+
+            customerManager.Save();
+        }
+
+        class CustomerManager
+        {
+            static CustomerManager _customerManager;
+            static object _locker = new object();
+            private CustomerManager()
+            {
+
+            }
+
+            public static CustomerManager CreateSingleton()
+            {
+                lock (_locker)
+                {
+                    if (_customerManager == null)
+                    {
+                        _customerManager = new CustomerManager();
+                    }
+                }
+
+                return _customerManager;
+                //return _customerManager ?? (_customerManager = new CustomerManager());
+            }
+
+            public void Save()
+            {
+                Console.WriteLine("Saved");
+            }
+        }
+    }
+}
