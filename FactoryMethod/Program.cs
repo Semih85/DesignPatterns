52ff39cff4c0fb8eb6fdb3f646fe723d97a7c79a
Semih Özdamar <semih.oezdamar@stud.tu-darmstadt.de>
2023-02-12 14:28:58 +0100

    a base example of factory desing pattern

--- FactoryMethod/Program.cs
+++ FactoryMethod/Program.cs
@@ -10,6 +10,76 @@ namespace FactoryMethod
     {
         static void Main(string[] args)
         {
+            CustomerManager manager = new CustomerManager(new LoggerFactory2());
+
+            manager.Save();
+
+            Console.ReadLine();
         }
     }
+
+    class LoggerFactory: ILoggerFactory
+    {
+        public ILogger CreateLogger()
+        {
+            //Business to decide factory
+            //duruma göre hangi logger'ın verileceğine kadar verilir.
+            return new SemLogger();
+        }
+    }
+
+    class LoggerFactory2 : ILoggerFactory
+    {
+        public ILogger CreateLogger()
+        {
+            //Business to decide factory
+            //duruma göre hangi logger'ın verileceğine kadar verilir.
+            return new Log4NetLogger();
+        }
+    }
+
+    internal interface ILoggerFactory
+    {
+        ILogger CreateLogger();
+    }
+
+    internal interface ILogger
+    {
+        void Log();
+    }
+
+    class SemLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged with SemLogger");
+        }
+    }
+
+    class Log4NetLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged with Log4NetLogger");
+        }
+    }
+
+    class CustomerManager
+    {
+        ILoggerFactory _loggerFactory;
+
+        public CustomerManager(ILoggerFactory loggerFactory)
+        {
+            _loggerFactory = loggerFactory;
+        }
+
+        public void Save()
+        {
+            Console.WriteLine("Saved!");
+            //ILogger logger = new LoggerFactory().CreateLogger();
+            ILogger logger = _loggerFactory.CreateLogger();
+            logger.Log();
+        }
+    }
+
 }
